82b0d372748b38c33ce42c401d21e9413c1b38e1
Francisco Salgueiro <92053465+franciscoBSalgueiro@users.noreply.github.com>
2022-01-13 12:28:41 +0000

    novo extra_ilhas_linha

--- projeto.pl
+++ projeto.pl
@@ -1,2 +1,15 @@
-extrai_ilhas_linha(N_L, Linha, Ilhas) :-
-    findall(ilha(Coluna,Pos), (Pos = (N_L,N_C), member(Coluna, Linha), Coluna\=0, nth1(N_C, Linha, Coluna)), Ilhas).
+extrai_ilhas_linha(N_L, Linha, Ilhas) :- extrai_ilhas_linha(N_L, Linha, Ilhas, 1).
+
+extrai_ilhas_linha(_, [], [], _).
+
+extrai_ilhas_linha(N_L, [Coluna | Linha], Ilhas, N_C) :-
+    Coluna\=0,
+    !,
+    N_C2 is N_C+1,
+    extrai_ilhas_linha(N_L, Linha, Ilhas2, N_C2),
+    append([ilha(Coluna,(N_L,N_C))], Ilhas2, Ilhas).
+
+extrai_ilhas_linha(N_L, [Coluna | Linha], Ilhas, N_C) :-
+    Coluna==0,
+    N_C2 is N_C+1,
+    extrai_ilhas_linha(N_L, Linha, Ilhas, N_C2).
