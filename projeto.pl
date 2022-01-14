67a18ae76ff9d6ca583c9b6a29f346981e41f08f
Francisco Salgueiro <fgcdbs@gmail.com>
2022-01-14 09:33:20 +0000

    parte do posicoes_entre

--- projeto.pl
+++ projeto.pl
@@ -13,3 +13,16 @@ extrai_ilhas_linha(N_L, [Coluna | Linha], Ilhas, N_C) :-
     Coluna==0,
     N_C2 is N_C+1,
     extrai_ilhas_linha(N_L, Linha, Ilhas, N_C2).
+
+posicoes_entre(Pos1, Pos2, Posicoes) :-
+    (PX1, PY1) = Pos1, 
+    (PX2, PY2) = Pos2,
+    (PX1==PX2 ; PY1==PY2),
+    findall((X,Y), 
+    (between(PX1, PX2, X), between(PY1, PY2, Y)),
+    Posicoes).
+
+% :- posicoes_entre((2,6), (1,6), Posicoes), writeln(Posicoes); writeln(false).
+% output: [(1,3),(1,4),(1,5)]
+:- posicoes_entre((2,6), (8,6), Posicoes), writeln(Posicoes); writeln(false).
+% output: [(3,6),(4,6),(5,6),(6,6),(7,6)]
